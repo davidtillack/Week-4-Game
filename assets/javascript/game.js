f62f88003cecce82700458bb0c7547f36f88b2a3
David <dstillac@ncsu.edu>
2018-02-11 19:44:37 -0500

    large push

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+// Set Global Variables
+var wins = 0;
+var losses = 0;
+var numberStorage = 0;
+
+function initializeGame(){
+	randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	console.log(randomNumber);
+	crystNumberRed = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	console.log(crystNumberRed);
+	crystNumberBlue = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	console.log(crystNumberBlue);
+	crystNumberBlack = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	console.log(crystNumberBlack);
+	crystNumberGreen = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+	console.log(crystNumberGreen); 
+	$("#randomNumber").html("The number to guess is: " + randomNumber);
+}
+initializeGame();
+
+function gameRules(){
+	if (randomNumber === numberStorage){
+		wins++;
+		alert('You won! Nice Work.');
+		$("#wins").html("" + wins);
+		$("#score").empty();
+		numberStorage = 0;
+		initializeGame();
+	
+	} else if (randomNumber < numberStorage){
+		losses++;
+		alert('Oops... You went over. Try again!');
+		$("#losses").html("" + losses);
+		$("#numberStorage").empty();
+		numberStorage = 0
+		initializeGame();
+	}
+};
+gameRules();
+
+$("#crystalOne").on("click",function(){
+	numberStorage += crystNumberRed;
+	$('#numberStorage').html("Your score is: " + numberStorage);
+	gameRules();
+});
+
+$("#crystalTwo").on("click",function(){
+	numberStorage += crystNumberBlue;
+	$('#numberStorage').html("Your score is: " + numberStorage);
+	gameRules();
+});
+
+$("#crystalThree").on("click",function(){
+	numberStorage += crystNumberBlack;
+	$('#numberStorage').html("Your score is: " + numberStorage);
+	gameRules();
+});
+
+$("#crystalFour").on("click",function(){
+	numberStorage += crystNumberGreen;
+	$('#numberStorage').html("Your score is: " + numberStorage);
+	gameRules();
+});
+
+
+
